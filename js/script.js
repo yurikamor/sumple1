30eb7271dfe8d0bf2ee3e45f8540e4c2cb06e7af
{yurikamori} <{cqt14x@gmail.com}>
2021-09-27 20:46:01 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  autoplaySpeed: 3000,
+  speed: 1000,
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 1,
+  slideToScroll: 1,
+  arrows: true,
+  prevAroow: '<div class="slick-prev"></div>',
+  nextAroow: '<div class="slick-next"></div>',
+})
